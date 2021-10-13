b35e41abe82780e148c6caf64905e1b33885bd59
Veerabrahmam Suthari <veerabrahmam.suthari@macys.com>
2021-10-13 12:54:11 -0500

    ADT Code Test

---
+++ ADTTest/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  ADTTest
+//
+//  Created by Veerabrahmam Suthari (contractor) on 10/13/21.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ADTTest/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  ADTTest
+//
+//  Created by Veerabrahmam Suthari (contractor) on 10/13/21.
+//
+
+import Foundation
